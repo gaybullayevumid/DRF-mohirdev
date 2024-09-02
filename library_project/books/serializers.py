0e58f899776d599c96dab8b55e78cad41299f9af
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-09-02 09:19:41 +0500

    feat: create serializer and connect view

--- library_project/books/serializers.py
+++ library_project/books/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Books
 
-class BookSerializers(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Books
--- library_project/books/views.py
+++ library_project/books/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Books
+from .serializers import BookSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+class BookListApi(generics.ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
